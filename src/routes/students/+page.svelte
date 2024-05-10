0b6e88c17542eaedd640c30ea65e835333132d6c
yakubmungai <yakub.mungai@ufl.edu>
2024-05-10 16:04:24 -0400

    Worked on layout for Students page

--- src/routes/students/+page.svelte
+++ src/routes/students/+page.svelte
@@ -1,1 +1,93 @@
-<p>students page. under construction...</p>
+<script>
+    // Enter Script here
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=iCiel+Gotham&family=Archer&display=swap');
+    .blue-rectangle {
+        width: 1041px;
+        height: 339px; /* Change height to auto to accommodate dynamic content */
+        top: 119px;
+        background-color: rgba(11, 15, 36, 1);
+        color: white;
+        opacity: 1;
+        position: absolute;
+        display: flex;
+        flex-direction: column; /* Set flex-direction to column to stack elements vertically */
+        padding: 20px; /* Add padding for inner content */
+    }
+
+    .get-involved-text {
+        width: 221px; 
+        height: 42px;
+        top: 166px;
+        left: 106px;
+        gap: 0px;
+        opacity: 1;
+        color: white;
+        font-family: 'iCiel Gotham', sans-serif;
+        font-size: 35px;
+        font-weight: 300;
+        line-height: 42px;
+        margin-bottom: 20px; 
+    }
+
+    .get-involved-paragraph-text {
+        width: 763px; 
+        height: 105px; 
+        top: 235px;
+        left: 106px;
+        gap: 0px;
+        opacity: 1;
+        color: white;
+        font-family: 'Archer';
+        font-size: 22px;
+        font-weight: 350;
+        line-height: 26.4px;
+    }
+
+    .orange-button {
+        width: 262px;
+        height: 52px;
+        top: 359px; 
+        left: 20px;
+        border-radius: 24px;
+        opacity: 1;
+        background-color: #FD9739;
+        color: black;
+        text-align: center;
+        line-height: 52px;
+        position: absolute;
+    }
+
+    .google-calendar-placeholder {
+        width: 1074px;
+        height: 984px;
+        margin: 581px auto;
+        left: 106px;
+        border: 1px solid black;
+        opacity: 1;
+        position: relative;
+    }
+</style>
+
+<div class="blue-rectangle">
+    <div class="get-involved-text">
+        Get Involved
+    </div>
+    <div class="get-involved-paragraph-text">
+        Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. 
+        Cras ut neque eget tellus blandit sollicitudin id ac risus. 
+        Vivamus erat arcu, ornare sed dolor eu, viverra placerat tellus. 
+        Quisque maximus eget sapien sed aliquam. Suspendisse vitae nisi ac
+    </div>
+</div>
+
+<div class="orange-button">
+    Subscribe to Calendar
+</div>
+
+<div class="google-calendar-placeholder">
+    <!-- Google Calendar stuff here -->
+</div>
