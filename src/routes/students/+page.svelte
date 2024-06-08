65e98e386af7f3f67c7d4c6aa3a1cfa5fdb7dc64
yakubmungai <yakub.mungai@ufl.edu>
2024-06-08 17:41:10 -0400

    Added the correct paragraph under the get involved section.

--- src/routes/students/+page.svelte
+++ src/routes/students/+page.svelte
@@ -7,16 +7,19 @@
     <div class="laptop:grid-cols-1 mt-6 laptop:gap-0 px-mobile-padding-x tablet:px-tablet-padding-x laptop:max-h-[600px] laptop:min-h-[420px] laptop:min-[1150px]:px-[50px] desktop:px-[0px]">
         <!-- Get Involved Section -->
         <div class="relative bg-body-background-blue rounded-md rounded-l-none flex flex-col p-16 max-w-[1041px] w-full h-auto laptop:top-[19px]">
-            <h2 class="text-white text-4xl font-gotham mb-6 -mt-2">Get Involved</h2>
+            <h2 class="text-white text-4xl font-gotham mb-6 -mt-2">
+                Get Involved
+            </h2>
             <p class="text-white text-xl font-archer mb-12">
-                Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit.
-                Cras ut neque eget tellus blandit sollicitudin id ac risus.
-                Vivamus erat arcu, ornare sed dolor eu, viverra placerat tellus.
-                Quisque maximus eget sapien sed aliquam. Suspendisse vitae nisi ac
+                The University of Florida ColorStack chapter is a dynamic community of over 170 Black and Latinx Computer Science students, 
+                offering year-round opportunities for community building, academic support, and career development. 
+                Through our Discord server, members exchange resources and forge lasting connections. 
+                Our events feature insightful sessions led by industry experts and provide invaluable networking opportunities. 
+                Additionally, we collaborate with top tech companies to offer mentorship, workshops, and enhanced job search support, giving our members a competitive edge in the industry. 
+                Join us and explore the multitude of opportunities to grow and excel in your academic and professional journey.
             </p>
             <a href="https://calendar.google.com/calendar/u/0/r?cid=MWZiODE4MTg2NGNkZjEyMDIyNjg3NDQ3Y2Y5YmNkNDk2OTgyZWZkNmQ3OTU2N2ZiMzI1M2Q4NzViM2M5YWJhN0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t" target="_blank" rel="noopener noreferrer">
-                <button class="bg-colorstackuf-orange text-black text-xl font-gotham hover:bg-colorstackuf-blue laptop:w-[262px] laptop:h-[52px] laptop:absolute laptop:top-[229px] laptop:left-[65px] laptop:gap-0 rounded-[24px]">
+                <button class="bg-colorstackuf-orange text-black text-xl font-gotham hover:bg-colorstackuf-blue laptop:w-[262px] laptop:h-[52px] laptop:absolute laptop:top-[340px] laptop:left-[65px] laptop:gap-0 rounded-[24px]">
                     Subscribe to Calendar
                 </button>
             </a>    
@@ -25,7 +28,7 @@
     
     
         <!-- Event Calendar Section -->
-        <div class="relative bg-body-background-white p-2 rounded-md flex flex-col mb-6 ml-[106px]">
+        <div class="relative bg-body-background-white p-2 rounded-md flex flex-col mb-2 mt-12 ml-[106px]">
             <h2 class="text-black text-4xl font-gotham">
                 Event Calendar
             </h2>
