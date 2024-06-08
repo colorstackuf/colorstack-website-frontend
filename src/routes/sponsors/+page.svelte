76d0cedce2354278d7491fb24020a6b08519582c
ejcleary <ejcleary03@gmail.com>
2024-06-07 21:37:11 -0400

    added photos

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -12,11 +12,11 @@
     import { type carouselImage } from '$lib/types';
 
     const bloombergImages: carouselImage[] = [
-    { id: 'fdl1', src: '/nvidia-fdl/fdl1.jpg', alt: 'Image 1' },
-    { id: 'fdl2', src: '/nvidia-fdl/fdl2.jpg', alt: 'Image 2' },
-    { id: 'fdl3', src: '/nvidia-fdl/fdl3.jpg', alt: 'Image 3' },
-    { id: 'fdl4', src: '/nvidia-fdl/fdl4.jpg', alt: 'Image 4' },
-    { id: 'fdl5', src: '/nvidia-fdl/fdl5.jpg', alt: 'Image 5' }
+    { id: 'Lab1', src: '/bloomberg/Lab1.jpeg', alt: 'Image 1' },
+    { id: 'Lab2', src: '/bloomberg/Lab2.jpeg', alt: 'Image 2' },
+    { id: 'Lab3', src: '/bloomberg/Lab3.jpeg', alt: 'Image 3' },
+    { id: 'Lab4', src: '/bloomberg/Lab4.jpeg', alt: 'Image 4' },
+    { id: 'Lab5', src: '/bloomberg/Lab5.jpeg', alt: 'Image 5' }
     ];
 
     const nvidiaImages: carouselImage[] = [
