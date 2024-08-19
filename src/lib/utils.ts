5f1902ed995f720fde4c559f0a58c626a5609116
yakubmungai <yakub.mungai@ufl.edu>
2024-08-19 17:53:51 -0400

    attempted to fix get involved animation triggering repeatedly

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,13 +2,15 @@ export function setAnimations() {
 	const observer = new IntersectionObserver((entries) => {
 		entries.forEach((entry) => {
 			// If the element is in view, add the visible class
-			if (entry.isIntersecting) {
+			if (entry.isIntersecting && entry.intersectionRatio == 1) {
 				entry.target.classList.add('slides-in--visible');
-			} else {
+			} else if (entry.intersectionRatio < 1) {
 				// Otherwise, remove the visible class
 				entry.target.classList.remove('slides-in--visible');
 			}
 		});
+	}, {
+		threshold: [0, 1] // Trigger when the element is fully in and out of view
 	});
 
 	document.querySelectorAll('.slides-in').forEach((elem) => {
