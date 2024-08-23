1ca1042d033c701e1a113c4f2e41fc8189a988a4
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-08-23 16:37:15 -0400

    format files and scale e-board container and images based on vw and vh

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -85,7 +85,7 @@
 			position: 'Founding President',
 			image: 'e-board/Carson_Johnson.png',
 			linkedin: 'https://www.linkedin.com/in/carson74johnson/'
-		},
+		}
 	];
 
 	let first_name = '';
@@ -117,45 +117,63 @@
 
 <div class="bg-body-background-white">
 	<!-- ColorStack UF intro -->
-	<div class="h-[40rem] h-auto laptop:h-auto lg-desktop:max-w-[1920px] mx-auto">
-		<div class="relative flex flex-col items-end laptop:items-start bg-[url('graphics/about-intro.png')] bg-cover bg-no-repeat bg-center h-[25rem] laptop:h-[45rem]">
-			<div class="relative grid grid-cols-1 top-[16rem] laptop:top-0 laptop:left-[3rem] laptop:top-[5rem] z-10 bg-body-background-blue pl-7 pr-4 py-7 laptop:p-12 rounded-l-lg laptop:rounded-lg mb-12 w-11/12 min-w-[310px] h-[380px] laptop:w-[460px] laptop:h-auto">
-				<h1 class="text-white text-lg tablet:text-2xl laptop:text-3xl lg-desktop:text-4xl font-gotham-medium laptop:pb-4 ">
+	<section class="h-[40rem] h-auto laptop:h-auto lg-desktop:max-w-[1920px] mx-auto">
+		<div
+			class="relative flex flex-col items-end laptop:items-start bg-[url('graphics/about-intro.png')] bg-cover bg-no-repeat bg-center h-[25rem] laptop:h-[45rem]"
+		>
+			<div
+				class="relative grid grid-cols-1 top-[16rem] laptop:top-0 laptop:left-[3rem] laptop:top-[5rem] z-10 bg-body-background-blue pl-7 pr-4 py-7 laptop:p-12 rounded-l-lg laptop:rounded-lg mb-12 w-11/12 min-w-[310px] h-[380px] laptop:w-[460px] laptop:h-auto"
+			>
+				<h1
+					class="text-white text-lg tablet:text-2xl laptop:text-3xl lg-desktop:text-4xl font-gotham-medium laptop:pb-4"
+				>
 					ColorStack UF is a team committed to...
 				</h1>
-				<p class="text-white font-archer text-lg tablet:text-xl leading-6 self-start lg-desktop:text-2xl">
-					Investing in the futures of those left behind every day. We’re a tech student organization
+				<p
+					class="text-white font-archer-light text-lg tablet:text-xl leading-6 self-start lg-desktop:text-2xl"
+				>
+					Investing in the futures of those left behind every day. We are a tech student organization
 					cultivating the strongest campus community of its kind to deliver community building,
 					academic support, and career development opportunities.
-                </p>
+				</p>
 				<a href="https://linktr.ee/colorstackuf" target="_blank" class="self-end laptop:self-start">
-					<div class="text-black w-44 text-center bg-colorstackuf-orange transition-bg-color transition-color hover:bg-colorstackuf-blue hover:text-white duration-300 py-3 px-7 rounded-3xl mt-8 font-gotham-medium">
+					<div
+						class="text-black w-44 text-center bg-colorstackuf-orange transition-bg-color transition-color hover:bg-colorstackuf-blue hover:text-white duration-300 py-3 px-7 rounded-3xl mt-8 font-gotham-medium"
+					>
 						View Link Tree
 					</div>
 				</a>
 			</div>
 		</div>
-	</div>
+	</section>
 
 	<!-- About ColorStack National -->
-	<div class="mt-60 laptop:mt-0">
-		<div class="laptop:bg-body-background-white bg-body-background-blue lg-desktop:max-w-[1920px] mx-auto laptop:py-40 py-6 px-8">
+	<section class="mt-60 laptop:mt-0">
+		<div
+			class="laptop:bg-body-background-white bg-body-background-blue max-w-[1920px] mx-auto laptop:py-40 py-6 px-8"
+		>
 			<div class="bg-body-background-blue rounded-lg laptop:p-12 desktop:p-16">
 				<div class="relative grid grid-cols-1 laptop:col-start-2 laptop:grid-cols-2 gap-x-12">
 					<img src="graphics/colorstack_students.png" alt="People" class="w-full rounded-lg" />
 
 					<div class="flex flex-col items-center text-center laptop:max-h-72">
-						<h1 class="text-white text-2xl desktop:text-4xl desktop:pt-8 pt-4 font-gotham-medium pb-4 desktop:pb-12">
+						<h1
+							class="text-white text-2xl desktop:text-4xl desktop:pt-8 pt-4 font-gotham-medium pb-4 desktop:pb-12"
+						>
 							About ColorStack National
 						</h1>
-						<p class="text-white font-archer text-sm laptop:text-base desktop:text-lg lg-desktop:text-2xl">
+						<p
+							class="text-white font-archer-light text-sm laptop:text-base desktop:text-lg lg-desktop:text-2xl"
+						>
 							The ColorStack Family is our community of 10,000+ Black and Latinx Computer Science
-							students from 900+ schools nationwide. We’re committed to providing community building,
-							academic support, and career development year-round through our active Slack channel,
-							monthly Fam Fridays, and partnerships with 70+ top tech companies.
+							students from 900+ schools nationwide. We’re committed to providing community
+							building, academic support, and career development year-round through our active Slack
+							channel, monthly Fam Fridays, and partnerships with 70+ top tech companies.
 						</p>
 						<a href="https://app.colorstack.io/apply" target="_blank">
-							<button class="text-black text-center bg-colorstackuf-orange transition-bg-color transition-color hover:bg-colorstackuf-blue hover:text-white duration-300 py-4 px-7 rounded-3xl mt-8 laptop:mt-12 font-gotham-medium lg-desktop:text-2xl">
+							<button
+								class="text-black text-center bg-colorstackuf-orange transition-bg-color transition-color hover:bg-colorstackuf-blue hover:text-white duration-300 py-4 px-7 rounded-3xl mt-8 laptop:mt-12 font-gotham-medium lg-desktop:text-2xl"
+							>
 								Become a National Member
 							</button>
 						</a>
@@ -163,56 +181,60 @@
 				</div>
 			</div>
 		</div>
-	</div>
+	</section>
 
-	<!-- Eboard Grid -->
-	<div class="bg-body-background-blue">
-		<div class="lg-desktop:max-w-[1920px] py-12">
-			<div class="flex flex-col justify-center items-center font-gotham-medium pb-12">
-				<h1 class="text-white text-[2rem] text-center lg-desktop:[3rem]">
-					Meet the E-Board
-				</h1>
-				<p class="text-white text-center pt-3 lg-desktop:text-2xl">
-					2024-2025
-				</p>
-			</div>
-			<div class="grid grid-cols-2 laptop:grid-cols-3 desktop:grid-cols-4 gap-y-12 laptop:px-12 px-6">
-				{#each eboard_members as member}
-					<div class="grid grid-cols-1 justify-items-center text-white text-[1.4rem] font-gotham-medium">
-						<div class="w-[35vw] h-[35vw] max-w-72 max-h-72">
-							<a href="{member.linkedin}" target="_blank" class="block w-full h-full">
-							  <img
-								src={member.image}
-								alt="Photo of {member.name}"
-								class="w-full h-full object-cover rounded-md group-hover:opacity-50 items-start"
-							  />
-							  <div class="absolute inset-0 flex items-center justify-center">
-								<img
-								  src="logos/linkedin.svg"
-								  alt="LinkedIn Logo"
-								  class="opacity-0 group-hover:opacity-75 transition-opacity duration-500 ease-in-out"
-								/>
-							  </div>
-							</a>
-						  </div>						  
-						<h1 class="py-3 desktop:py-5 text-center text-xl tablet:text-2xl lg-desktop:text-3xl">{member.name}</h1>
-						<p class="text-xs desktop:text-lg lg-desktop:text-xl text-center laptop:text-base">{member.position}</p>
-					</div>
-				{/each}
-			</div>
+	<!-- Eboard -->
+	<section class="bg-body-background-blue flex flex-col items-center">
+		<div class="w-full max-w-[1920px] flex flex-col justify-center font-gotham-medium py-12 gap-y-4 px-mobile-padding-x tablet:px-tablet-padding-x laptop:px-laptop-padding-x">
+				<h1 class="text-white text-[2rem] text-center lg-desktop:[3rem]">Meet the E-Board</h1>
+				<p class="text-white text-center pb-4 lg-desktop:text-2xl">2024-2025</p>
+
+				<div
+					class="grid grid-cols-2 tablet:grid-cols-3 desktop:grid-cols-4 gap-x-16 gap-y-6 tablet:gap-y-10 laptop:px-12 px-6"
+				>
+					{#each eboard_members as member}
+						<!-- Member -->
+						<div
+							class="grid grid-cols-1 justify-items-center text-white text-[1.4rem] font-gotham-medium w-fit"
+						>
+							<!-- Image -->
+							<div class="w-[44vw] h-[44vw] tablet:w-[27vw] desktop:w-[20vw] max-w-[500px] desktop:max-w-[400px] tablet:h-[27vw] desktop:h-[20vw] max-h-[500px] desktop:max-h-[400px] mb-2 notebook:mb-4">
+								<a href={member.linkedin} target="_blank" class="block w-full h-full">
+									<img
+										src={member.image}
+										alt="Photo of {member.name}"
+										class="w-full h-full object-cover rounded-lg laptop:rounded-xl group-hover:opacity-50 items-start"
+									/>
+								<!-- 	<div class="absolute inset-0 flex items-center justify-center"> -->
+								<!-- 		<img -->
+								<!-- 			src="logos/linkedin.svg" -->
+								<!-- 			alt="LinkedIn Logo" -->
+								<!-- 			class="opacity-0 group-hover:opacity-75 transition-opacity duration-500 ease-in-out" -->
+								<!-- 		/> -->
+								<!-- 	</div> -->
+								</a>
+							</div>
+
+							<h1 class="text-center text-[3vw] tablet:text-[2.2vw] desktop:text-[min(1.8vw,2rem)] leading-6 tablet:leading-8 desktop:leading-10">
+								{member.name}
+							</h1>
+							<p class="font-gotham-light text-center text-[1.8vw] min-[360px]:text-[1.9vw] tablet:text-[min(1.37vw,0.9rem)] desktop:text-[min(1.125vw,1.3rem)]">
+								{member.position}
+							</p>
+						</div>
+					{/each}
+				</div>
 		</div>
-	</div>
+	</section>
 </div>
 
-<div class="bg-body-background-blue">
+<section class="bg-body-background-blue">
 	<!-- Contact Us -->
 	<div class="bg-body-background-light-blue">
 		<div class="lg-desktop:max-w-[1920px] mx-auto laptop:px-24 laptop:py-36 py-12 px-12 mb-7">
 			<div class="grid grid-cols-1 laptop:grid-cols-2 laptop:cols-start-2 gap-12">
 				<div class="text-black">
-					<h1 class="text-3xl lg-desktop:text-5xl pb-8 font-gotham-medium">
-						Contact Us
-					</h1>
+					<h1 class="text-3xl lg-desktop:text-5xl pb-8 font-gotham-medium">Contact Us</h1>
 					<p class="text-2xl lg-desktop:text-4xl font-archer">
 						If you have any questions or concerns don't hesitate to contact us!
 					</p>
@@ -244,11 +266,12 @@
 					/>
 					<button
 						class="w-full text-white bg-body-background-blue transition-bg-color transition-color hover:bg-colorstackuf-orange hover:text-black duration-300 py-4 px-7 rounded mt-4 font-gotham-medium"
-						on:click={submitForm}>
+						on:click={submitForm}
+					>
 						Submit
 					</button>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+</section>
