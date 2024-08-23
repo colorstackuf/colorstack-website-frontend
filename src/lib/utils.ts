f57cb4650a932b85cf8471f9fb4648d59a35874d
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-08-23 19:25:43 -0400

    fix transition bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,15 +2,13 @@ export function setAnimations() {
 	const observer = new IntersectionObserver((entries) => {
 		entries.forEach((entry) => {
 			// If the element is in view, add the visible class
-			if (entry.isIntersecting && entry.intersectionRatio == 1) {
+			if (entry.isIntersecting) {
 				entry.target.classList.add('slides-in--visible');
-			} else if (entry.intersectionRatio < 1) {
+			} else {
 				// Otherwise, remove the visible class
 				entry.target.classList.remove('slides-in--visible');
 			}
 		});
-	}, {
-		threshold: [0, 1] // Trigger when the element is fully in and out of view
 	});
 
 	document.querySelectorAll('.slides-in').forEach((elem) => {
